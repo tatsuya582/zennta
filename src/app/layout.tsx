7b8f8a79254a5d14d4117fcbf84ad50eee6b3a48
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-22 12:23:15 +0900

    add: metadataを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,26 @@ import "./globals.css";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Zennta",
+  title: {
+    template: "%s | Zennta",
+    default: "Zennta"
+  },
   description:
     "ZennやQiitaのAPIを利用して記事一覧や検索を同時にできるサービスです。読んだ履歴や後で読む、お気に入りなどができます。",
+  openGraph: {
+    title: "Zennta",
+    description:
+      "ZennやQiitaのAPIを利用して記事一覧や検索を同時にできるサービスです。読んだ履歴や後で読む、お気に入りなどができます。",
+    type: "website",
+    url: "https://zennta.vercel.app/",
+    locale: "ja_JP",
+  },
+  twitter: {
+    card: "summary",
+    title: "Zennta",
+    description:
+      "ZennやQiitaのAPIを利用して記事一覧や検索を同時にできるサービスです。読んだ履歴や後で読む、お気に入りなどができます。",
+  },
 };
 
 export default function RootLayout({
