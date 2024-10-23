47d8865606faa403ab958fcb4961df20c1552327
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-23 22:03:18 +0900

    add: headerテスト仮作成

---
+++ src/__test__/components/Header.test.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/layout/header/header";
+import { render, screen } from "@testing-library/react";
+
+describe("Header Component", () => {
+  test("renders the logo with the correct text", () => {
+    render(<Header />);
+    const logoElement = screen.getByText(/Zennta/i);
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  test("renders navigation links correctly", () => {
+    render(<Header />);
+    const navLinks = screen.getAllByRole("link");
+    expect(navLinks.length).toBeGreaterThan(0);
+  });
+});
