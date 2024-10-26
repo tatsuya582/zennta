0ebe0abcaec13586d19629710abe5fb25ccc3150
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-26 17:40:52 +0900

    add: 一覧ページを仮作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <div className="w-full ml-auto md:w-3/4 p-12 md:mt-24 mt-12">ホーム</div>;
+  return (
+    <div className="w-full ml-auto md:w-3/4 p-12 md:mt-24 mt-12">
+      <div className="w-full flex justify-center items-center flex-col m-2">
+        <h2>Qiita一覧</h2>
+        <div className="w-full border rounded-lg p-2 mt-2 border-gray-300">List</div>
+      </div>
+
+      <div className="w-full flex justify-center items-center flex-col m-2 mt-8">
+        <h2>Zenn一覧</h2>
+        <div className="w-full border rounded-lg p-2 mt-2 border-gray-300">List</div>
+      </div>
+    </div>);
 }
