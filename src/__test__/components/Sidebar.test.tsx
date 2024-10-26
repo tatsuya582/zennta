6dfb2282504645eb6ddc2c4ae59ae894c9da6f66
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-26 17:49:40 +0900

    fix: decribeを変更

--- src/__test__/components/Sidebar.test.tsx
+++ src/__test__/components/Sidebar.test.tsx
@@ -1,7 +1,7 @@
 import Sidebar from "@/components/layout/sidebar/Sidebar";
 import { render, screen } from "@testing-library/react";
 
-describe("Header Component", () => {
+describe("Sidebar Component", () => {
   test("renders the history section with the correct text", () => {
     render(<Sidebar />);
     const historyHeading = screen.getByText(/履歴/i);
