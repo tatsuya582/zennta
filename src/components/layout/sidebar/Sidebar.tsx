a747f3645555201a037000fb42a2b89921a6025b
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-26 17:40:34 +0900

    fix: スペースが多かったのを修正

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 export default function Sidebar() {
   return (
     <>
-      <aside className="sidebar md:flex hidden w-1/4 max-w-xs  overflow-y-auto h-[calc(100vh-95px)] fixed top-24 border-r border-gray-300">
+      <aside className="sidebar md:flex hidden w-1/4 max-w-xs overflow-y-auto h-[calc(100vh-95px)] fixed top-24 border-r border-gray-300">
         <div className="w-full p-4">
           <h2>履歴</h2>
         </div>
