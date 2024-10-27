786f102dcccad124fdf57e4f2c43135d62dfdaf5
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-27 22:44:02 +0900

    add: signupページにもログイン前でも行けるように

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -37,6 +37,7 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/signup") &&
     request.nextUrl.pathname !== "/"
   ) {
     // no user, potentially respond by redirecting the user to the login page
