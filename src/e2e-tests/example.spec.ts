57af6c2320d634238a8dc4daec0e624cfc6a1f88
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-27 16:37:53 +0900

    add: signupPageのe2eテストを仮作成

--- src/e2e-tests/example.spec.ts
+++ src/e2e-tests/example.spec.ts
@@ -25,3 +25,13 @@ test("should display Qiita and Zenn sections on the homepage", async ({ page }) 
   await expect(qiitaSection).toBeVisible();
   await expect(zennSection).toBeVisible();
 });
+
+test("should render signup page elements", async ({ page }) => {
+  await page.goto("/signup");
+  const gitHubBottun = await page.locator('button:has-text("GitHubで会員登録")');
+  const googleBottun = await page.locator('button:has-text("Googleで会員登録")');
+  const xBottun = await page.locator('button:has-text("Xで会員登録")');
+  await expect(gitHubBottun).toBeVisible();
+  await expect(googleBottun).toBeVisible();
+  await expect(xBottun).toBeVisible();
+});
