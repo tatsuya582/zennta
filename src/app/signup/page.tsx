f9c4813fb577f063743b1dd1e40eaa96573a9649
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-27 16:24:36 +0900

    fix: signuppageのwidthを調整

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function SignupPage() {
   return (
     <div className="flex justify-center mt-12">
-      <div className="flex flex-col gap-6 items-center md:w-1/2 w-full p-12 md:mt-2 mt-6 border rounded-lg border-gray-300">
+      <div className="flex flex-col gap-6 items-center md:w-2/3 max-w-lg w-full p-12 md:mt-2 mt-6 border rounded-lg border-gray-300">
         <h2 className="text-center">会員登録</h2>
         <div className="flex flex-col gap-6">
           <Button size="lg">GitHubで会員登録</Button>
