b390cd3770d6fa12ab1575eeea07347ffb97363c
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-27 16:20:06 +0900

    fix: Signup()ではなくSignupPage()に変更

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,14 @@
 import { Button } from "@/components/ui/button";
 
-export default function Signup() {
+export default function SignupPage() {
   return (
     <div className="flex justify-center mt-12">
       <div className="flex flex-col gap-6 items-center md:w-1/2 w-full p-12 md:mt-2 mt-6 border rounded-lg border-gray-300">
         <h2 className="text-center">会員登録</h2>
         <div className="flex flex-col gap-6">
           <Button size="lg">GitHubで会員登録</Button>
-          <Button>Googleで会員登録</Button>
-          <Button>Xで会員登録</Button>
+          <Button size="lg">Googleで会員登録</Button>
+          <Button size="lg">Xで会員登録</Button>
         </div>
       </div>
     </div>
