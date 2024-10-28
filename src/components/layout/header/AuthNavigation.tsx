0b865eb40a1bb775f2d0cd2a96e2d6093bb3409e
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-10-28 18:00:52 +0900

    add: suspenseを利用

--- src/components/layout/header/AuthNavigation.tsx
+++ src/components/layout/header/AuthNavigation.tsx
@@ -8,10 +8,13 @@ import {
 import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 import { logout } from "@/actions/auth";
+import { Suspense } from "react";
 
 export default async function AuthNavigation() {
   const supabase = await createClient();
-  const { data: { user } } = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
   return (
     <>
@@ -19,22 +22,24 @@ export default async function AuthNavigation() {
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              {user ? (
-                <form action={logout}>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <button type="submit">ログアウト</button>
-                  </NavigationMenuLink>
-                </form>
-              ) : (
-                <>
-                  <Link href="/" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>ログイン</NavigationMenuLink>
-                  </Link>
-                  <Link href="/signup" legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>会員登録</NavigationMenuLink>
-                  </Link>
-                </>
-              )}
+              <Suspense fallback={<div>Loading...</div>}>
+                {user ? (
+                  <form action={logout}>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                      <button type="submit">ログアウト</button>
+                    </NavigationMenuLink>
+                  </form>
+                ) : (
+                  <>
+                    <Link href="/" legacyBehavior passHref>
+                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>ログイン</NavigationMenuLink>
+                    </Link>
+                    <Link href="/signup" legacyBehavior passHref>
+                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>会員登録</NavigationMenuLink>
+                    </Link>
+                  </>
+                )}
+              </Suspense>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
