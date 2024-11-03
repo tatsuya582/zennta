1d3cfa59bc3923f87ae5f3f8e20125c3f76a131f
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-03 17:03:01 +0900

    add: loginページのe2eテストを作成

--- src/e2e-tests/signuppage.spec.ts
+++ src/e2e-tests/signuppage.spec.ts
@@ -7,7 +7,7 @@ test("should render signup page elements", async ({ page }) => {
   await expect(page.locator('button:has-text("Xで会員登録")')).toBeVisible();
 });
 
-test("should link", async ({ page }) => {
+test("should link main page", async ({ page }) => {
   await page.goto("/signup");
   await page.locator("text=Zennta").click();
   await expect(page.locator("text=Qiita一覧")).toBeVisible();
