cc787ad80cbc04fbc2cad974275dcb7255145665
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-03 15:42:21 +0900

    fix: e2eテストリファクタリング

--- src/e2e-tests/signuppage.spec.ts
+++ src/e2e-tests/signuppage.spec.ts
@@ -2,10 +2,13 @@ import test, { expect } from "@playwright/test";
 
 test("should render signup page elements", async ({ page }) => {
   await page.goto("/signup");
-  const gitHubBottun = await page.locator('button:has-text("GitHubで会員登録")');
-  const googleBottun = await page.locator('button:has-text("Googleで会員登録")');
-  const xBottun = await page.locator('button:has-text("Xで会員登録")');
-  await expect(gitHubBottun).toBeVisible();
-  await expect(googleBottun).toBeVisible();
-  await expect(xBottun).toBeVisible();
+  await expect(page.locator('button:has-text("GitHubで会員登録")')).toBeVisible();
+  await expect(page.locator('button:has-text("Googleで会員登録")')).toBeVisible();
+  await expect(page.locator('button:has-text("Xで会員登録")')).toBeVisible();
+});
+
+test("should link", async ({ page }) => {
+  await page.goto("/signup");
+  await page.locator("text=Zennta").click();
+  await expect(page.locator("text=Qiita一覧")).toBeVisible();
 });
