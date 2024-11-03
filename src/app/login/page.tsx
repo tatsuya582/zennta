69d546a19b979e21c9c208d26419d8bd11feb13c
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-03 16:49:31 +0900

    fix: ログインと会員登録ページがほとんど同じなのでリファクタリング

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,5 @@
-import { SigninButton } from "@/components/ui/signinButton";
+import { AuthForm } from "@/components/ui/authForm";
 
 export default function LoginPage() {
-  return (
-    <div className="flex justify-center mt-12">
-      <div className="flex flex-col gap-6 items-center md:w-2/3 max-w-lg w-full p-12 md:mt-2 mt-6 border rounded-lg border-gray-300">
-        <h2 className="text-center">ログイン</h2>
-        <div className="flex flex-col gap-6">
-          <SigninButton provider="github">GitHubでログイン</SigninButton>
-          <SigninButton provider="google">Googleでログイン</SigninButton>
-          <SigninButton provider="twitter">Xでログイン</SigninButton>
-        </div>
-      </div>
-    </div>
-  );
+  return <AuthForm type="login" />;
 }
---
+++ src/components/ui/authForm.tsx
@@ -0,0 +1,20 @@
+import { AuthButton } from "@/components/ui/authButton";
+
+export const AuthForm = ({ type }: { type: string }) => {
+  const isLogin = type === "login";
+  const title = isLogin ? "ログイン" : "会員登録";
+  const buttonText = isLogin ? "でログイン" : "で会員登録";
+
+  return (
+    <div className="flex justify-center mt-12">
+      <div className="flex flex-col gap-6 items-center md:w-2/3 max-w-lg w-full p-12 md:mt-2 mt-6 border rounded-lg border-gray-300">
+        <h2 className="text-center">{title}</h2>
+        <div className="flex flex-col gap-6">
+          <AuthButton provider="github">GitHub{buttonText}</AuthButton>
+          <AuthButton provider="google">Google{buttonText}</AuthButton>
+          <AuthButton provider="twitter">X{buttonText}</AuthButton>
+        </div>
+      </div>
+    </div>
+  );
+};
