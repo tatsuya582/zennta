ff03bd35e50aabd5f708dd8725aebd84067e9b3e
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-05 15:12:23 +0900

    add: データベースのtypesを作成

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,5 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
+import { type Database } from '@/types/database.types'
 
 export function createClient() {
-  return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+  return createBrowserClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 }
