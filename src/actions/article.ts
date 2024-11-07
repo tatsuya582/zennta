f3483520bf56ed58eacfa91beca9198af331f886
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-07 23:39:27 +0900

    add: Zenn記事の一覧取得用メソッドを作成

--- src/actions/article.ts
+++ src/actions/article.ts
@@ -43,3 +43,19 @@ export const searchQiitaArticles = async ({ page, query }: { page: string; query
     throw new Error("Failed to fetch articles");
   }
 };
+
+export const getZennArticles = async ({ page }: { page: string }) => {
+  try {
+    const response = await fetch(`https://zenn.dev/api/articles?page=${page}&order=latest`);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching Qiita items:", error);
+    throw new Error("Failed to fetch articles");
+  }
+};
