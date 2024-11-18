cbeec532568eb085ab785deea5d99d6fbb4227bf
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-18 19:54:01 +0900

    add: ヒストリーが1人20件になるようにトリガーを作成

---
+++ supabase/migrations/20241118105231_remote_schema.sql
@@ -0,0 +1,20 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.limit_histories_per_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$BEGIN
+  DELETE FROM histories
+  WHERE id IN (
+    SELECT id FROM histories
+    WHERE "userId" = NEW."userId"
+    ORDER BY "updatedAt" DESC
+    OFFSET 20
+  );
+  RETURN NEW;
+END;$function$
+;
+
+CREATE TRIGGER limit_histories_trigger AFTER INSERT ON public.histories FOR EACH ROW EXECUTE FUNCTION limit_histories_per_user();
+
+
