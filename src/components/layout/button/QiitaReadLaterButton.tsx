a3d98348783970cf0966db7c4f067678ccbcd87e
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-19 21:53:19 +0900

    add: ボタンをuseStateを使うように変更
    デザインは後でやる

--- src/components/layout/button/QiitaReadLaterButton.tsx
+++ src/components/layout/button/QiitaReadLaterButton.tsx
@@ -3,23 +3,40 @@
 import { addreadLaterQiita } from "@/actions/readLater";
 import { Button } from "@/components/ui/button";
 import { QiitaItem } from "@/types/types";
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const QiitaReadLaterButton = ({ item, readLaterUrls }: { item: QiitaItem; readLaterUrls: Set<unknown> }) => {
-  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isReadLater, setIsReadLater] = useState(readLaterUrls.has(item.url))
   const onSubmit = async (item: QiitaItem) => {
-    await addreadLaterQiita(item);
-    router.refresh();
+    try {
+      setIsLoading(true)
+      await addreadLaterQiita(item);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsReadLater(!isReadLater);
+      setIsLoading(false);
+    }
   };
-  const isReadLater = readLaterUrls.has(item.url);
   return (
     <div>
       {isReadLater ? (
-        <Button variant="outline">登録済み</Button>
+        <div>
+          {isLoading ? (
+            <Button variant="outline"><span className="loader mx-1"></span></Button>
+          ): (
+            <Button variant="outline">登録済み</Button>
+          )}
+        </div>
       ) : (
-        <Button variant="outline" onClick={() => onSubmit(item)}>
-          後で読む
-        </Button>
+        <div>
+          {isLoading ? (
+            <Button variant="outline" onClick={(e) => e.preventDefault()}><span className="loader mx-1"></span></Button>
+          ): (
+            <Button variant="outline" onClick={() => onSubmit(item)}>後で読む</Button>
+          )}
+        </div>
       )}
     </div>
   );
