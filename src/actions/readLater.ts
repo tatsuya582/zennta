0ebc96ef4fc04d14d9e146dfd057e7632a90fad4
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-19 17:22:49 +0900

    add: addreadLaterQiitaメソッドを作成

---
+++ src/actions/readLater.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { currentUser } from "@/lib/auth/currentUser/server";
+import { type QiitaItem } from "@/types/types";
+import { createClient } from "@/utils/supabase/server";
+
+export const addreadLaterQiita = async (item: QiitaItem) => {
+  try {
+    const supabase = await createClient();
+    const user = await currentUser();
+
+    if (!user) {
+      return null;
+    }
+
+    const { error } = await supabase.rpc("insert_read_later_with_article", {
+      articleprovider: "Qiita",
+      articlesourcecreatedat: item.created_at,
+      articletitle: item.title,
+      articleurl: item.url,
+      tags: item.tags,
+      userid: user.id,
+    });
+
+    if (error) {
+      console.error("Error adding article:", error);
+      throw error;
+    }
+  } catch (err) {
+    console.error("Unexpected error:", err);
+    throw err;
+  }
+};
