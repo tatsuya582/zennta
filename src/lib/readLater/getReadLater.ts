d58892e541e319baeb12009f606e57ecc5084f70
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-20 21:41:54 +0900

    add: Zennにも後で読むボタンを追加

--- src/lib/readLater/getReadLater.ts
+++ src/lib/readLater/getReadLater.ts
@@ -1,4 +1,4 @@
-import { QiitaItem } from "@/types/types";
+import { QiitaItem, ZennItem } from "@/types/types";
 
 export const getQiitaCreatedAtRange = (articles: QiitaItem[]): { start: string | null; end: string | null } => {
   if (articles.length === 0) {
@@ -7,6 +7,17 @@ export const getQiitaCreatedAtRange = (articles: QiitaItem[]): { start: string |
 
   const start = articles[articles.length - 1].created_at;
   const end = articles[0].created_at;
+
+  return { start, end };
+};
+
+export const getZennCreatedAtRange = (articles: ZennItem[]): { start: string | null; end: string | null } => {
+  if (articles.length === 0) {
+    return { start: null, end: null };
+  }
+
+  const start = articles[articles.length - 1].published_at;
+  const end = articles[0].published_at;
 
   return { start, end };
 };
