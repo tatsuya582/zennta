af0d53fc9a8403c21f4bfce26611c77fd9cad402
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-21 18:15:05 +0900

    add: 後で読むページを仮作成

---
+++ src/app/readlater/page.tsx
@@ -0,0 +1,25 @@
+import { getReadLaterArticles } from "@/actions/readLater";
+import ArticleList from "@/components/layout/readLater/ArticleList";
+
+export default async function ReadLaterPage({
+  searchParams,
+}: {
+  searchParams?: {
+    page?: string;
+  };
+}) {
+  const page = searchParams?.page ? parseInt(searchParams.page, 10) || 1 : 1;
+  // const articles = await getReadLaterArticles(page);
+
+  return (
+    <>
+      <div className="w-full flex justify-center items-center flex-col md:mt-2 mt-8">
+        <h2>後で読む</h2>
+        <div className="w-full md:border border-y md:rounded-lg rounded-none p-2 mt-2 border-gray-300">
+          {/* {articles?.map((item) => <div key={item.articles.id}>{item.articles.title}</div>)} */}
+          <ArticleList page={page} />
+        </div>
+      </div>
+    </>
+  );
+}
