2ea2a4877bd57e761af4f2b8c588f7c05659d572
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-25 17:36:01 +0900

    fix: QiitaItemのtagsがnullを許容するように

--- src/types/types.ts
+++ src/types/types.ts
@@ -15,7 +15,7 @@ export type QiitaItem = {
   id: string;
   title: string;
   url: string;
-  tags: Tag[];
+  tags: Tag[] | null;
   created_at: string; // ISO 8601 format, e.g., "2000-01-01T00:00:00+00:00"
 };
 
