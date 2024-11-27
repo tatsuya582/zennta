c9cd13525109ad6e94b1ef898c3040f4d24105ba
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-27 16:40:25 +0900

    add: 後で読むページにsuspenseを追加

--- src/app/readlater/page.tsx
+++ src/app/readlater/page.tsx
@@ -1,4 +1,6 @@
 import ReadLsterArticleList from "@/components/layout/readLater/ReadLaterArticleList";
+import QiitaArticleListSkeleton from "@/components/layout/skeleton/QiitaArticleListSkeleton";
+import { Suspense } from "react";
 
 export default async function ReadLaterPage({
   searchParams,
@@ -14,7 +16,9 @@ export default async function ReadLaterPage({
       <div className="w-full flex justify-center items-center flex-col md:mt-2 mt-8 mb-4">
         <h2>後で読む</h2>
         <div className="w-full md:border border-y md:rounded-lg rounded-none p-2 mt-2 border-gray-300">
-          <ReadLsterArticleList page={page} />
+          <Suspense key={JSON.stringify(searchParams)} fallback={<QiitaArticleListSkeleton />}>
+            <ReadLsterArticleList page={page} />
+          </Suspense>
         </div>
       </div>
     </>
