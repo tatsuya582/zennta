304b018c88d77e48fb14b13303a346efa5261b23
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-27 18:01:02 +0900

    add: favoriteで使いそうなメソッドを仮作成

---
+++ src/actions/favorite.ts
@@ -0,0 +1,163 @@
+"use server";
+
+import { currentUser } from "@/lib/auth/currentUser/server";
+import { FetchedArticle, FetchedArticles } from "@/types/databaseCustom.types";
+import { StoredItem, type FetchedItem } from "@/types/types";
+import { createClient } from "@/utils/supabase/server";
+
+export const addFavorite = async (item: FetchedItem) => {
+  try {
+    const supabase = await createClient();
+    const user = await currentUser();
+
+    if (!user) {
+      return null;
+    }
+
+    const { data, error } = await supabase.rpc("insert_favorite_with_article", {
+      userid: user.id,
+      articleurl: item.url,
+      articletitle: item.title,
+      articlesourcecreatedat: item.created_at,
+      tags: item.tags,
+    });
+
+    if (error) {
+      console.error("Error adding article:", error);
+      throw error;
+    }
+
+    return data;
+  } catch (err) {
+    console.error("Unexpected error:", err);
+    throw err;
+  }
+};
+
+export const addStoredFavorite = async (item: StoredItem) => {
+  try {
+    const supabase = await createClient();
+    const user = await currentUser();
+
+    if (!user) {
+      return null;
+    }
+
+    const { error } = await supabase.from("favorites").insert({ userId: user.id, articleId: item.id });
+
+    if (error) {
+      console.error("Error adding article:", error);
+      throw error;
+    }
+  } catch (err) {
+    console.error("Unexpected error:", err);
+    throw err;
+  }
+};
+
+export const getFavorite = async (start: string, end: string): Promise<Map<string | undefined, string | undefined>> => {
+  try {
+    const supabase = await createClient();
+    const user = await currentUser();
+
+    if (!user) {
+      return new Map();
+    }
+
+    const { data } = (await supabase
+      .from("favorites")
+      .select(`articles:articleId (id, sourceCreatedAt, url)`)
+      .eq("userId", user.id)
+      .gte("articles.sourceCreatedAt", start)
+      .lte("articles.sourceCreatedAt", end)
+      .not("articles", "is", null)) as unknown as { data: FetchedArticle[] };
+
+    if (!data) {
+      return new Map();
+    }
+
+    const favoriteMap = new Map(data.map((item) => [item.articles?.url, item.articles?.id]));
+    return favoriteMap;
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    throw error;
+  }
+};
+
+export const getFavoriteHistory = async (): Promise<Map<string | undefined, string | undefined>> => {
+  try {
+    const supabase = await createClient();
+    const user = await currentUser();
+
+    if (!user) {
+      return new Map();
+    }
+
+    const { data } = (await supabase
+      .from("favorites")
+      .select(`articles:articleId (id, url)`)
+      .eq("userId", user.id)
+      .not("articles", "is", null)) as unknown as { data: FetchedArticle[] };
+
+    if (!data) {
+      return new Map();
+    }
+
+    const favoriteMap = new Map(data.map((item) => [item.articles?.url, item.articles?.id]));
+    return favoriteMap;
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    throw error;
+  }
+};
+
+export const deleteFavorite = async (articleId: string) => {
+  try {
+    const supabase = await createClient();
+    const user = await currentUser();
+
+    if (!user) {
+      return null;
+    }
+
+    const { error } = await supabase.from("favorites").delete().eq("userId", user.id).eq("articleId", articleId);
+
+    if (error) {
+      throw new Error(`Failed to delete record: ${error.message}`);
+    }
+  } catch (error) {
+    console.error("Error deleting favorite entry:", error);
+  }
+};
+
+export const getFavoriteArticles = async (page: number) => {
+  try {
+    const supabase = await createClient();
+    const user = await currentUser();
+    const start = 30 * (page - 1);
+    const end = 30 * page - 1;
+
+    if (!user) {
+      return null;
+    }
+
+    const { data, count } = (await supabase
+      .from("favorites")
+      .select(
+        `
+        articles:articleId (id, title, url, tags)
+        `,
+        { count: "exact" }
+      )
+      .eq("userId", user.id)
+      .order("createdAt", { ascending: false })
+      .range(start, end)) as unknown as { data: FetchedArticles[]; count: number | null };
+
+    const totalPage = count !== null ? Math.ceil(count / 30) : 1;
+
+    return { articles: data, totalPage };
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    throw error;
+  }
+};
