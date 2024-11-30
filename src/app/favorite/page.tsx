63c626819e8f9b874c165d56720e5bb1c3434e5b
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-11-30 21:45:45 +0900

    add: お気に入りページ検索機能を追加

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,6 +1,9 @@
 import { getFavoriteArticles } from "@/actions/favorite";
 import StoredArticleList from "@/components/layout/main/StoredArticleList";
 import QiitaArticleListSkeleton from "@/components/layout/skeleton/QiitaArticleListSkeleton";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export default async function FavoritePage({
@@ -13,10 +16,23 @@ export default async function FavoritePage({
 }) {
   const page = searchParams?.page ? parseInt(searchParams.page, 10) || 1 : 1;
   const query = searchParams?.query ? searchParams.query : undefined;
-  const buildHref = (pageNumber: number) => `/favorite?page=${pageNumber}`;
+  const queryParam = query ? `query=${encodeURIComponent(query)}&` : "";
+  const buildHref = (pageNumber: number) => `/favorite?${queryParam}page=${pageNumber}`;
+  const onsubmit = async (formData: FormData) => {
+    "use server";
+    const name = formData.get("name")?.toString().trim() || "";
+    const encodedName = encodeURIComponent(name);
+    redirect(`/favorite?query=${encodedName}`);
+  };
 
   return (
     <>
+      <div className="flex items-center justify-center my-12">
+        <form action={onsubmit} className="flex gap-4 w-9/12 max-w-screen-sm">
+          <Input type="name" name="name" defaultValue={query || ""} />
+          <Button>検索</Button>
+        </form>
+      </div>
       <div className="w-full flex justify-center items-center flex-col md:mt-2 mt-8 mb-4">
         <h2>お気に入り</h2>
         <div className="w-full md:border border-y md:rounded-lg rounded-none p-2 mt-2 border-gray-300">
