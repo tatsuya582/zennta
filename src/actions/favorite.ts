e998e4e6eedf3fa92d6f70347abe802f7af42bd9
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-01 14:21:19 +0900

    add: タグとメモを追加するメソッドを作成

--- src/actions/favorite.ts
+++ src/actions/favorite.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import { addArticle, deleteArticle, getArticle, getArticleHistory, getArticles } from "@/actions/storedArticle";
-import { type StoredItem, type FetchedItem } from "@/types/types";
+import { getSupabaseClientAndUser } from "@/lib/supabase/server";
+import { type StoredItem, type FetchedItem, Tag } from "@/types/types";
 
 export const addFavorite = async (item: FetchedItem) => {
   addArticle("favorites", item, "insert_favorite_with_article");
@@ -25,4 +26,23 @@ export const deleteFavorite = async (articleId: string) => {
 
 export const getFavoriteArticles = async (page: number, query: string | undefined) => {
   return getArticles("fetch_favorites_articles_with_count", page, query);
+};
+
+export const updateFavoriteColumn = async (id: string, value: string | Tag[]) => {
+  try {
+    const { supabase } = await getSupabaseClientAndUser();
+    const column = typeof value === "string" ? "memo" : "tags";
+
+    const { error } = await supabase
+      .from("favorites")
+      .update({ [column]: value })
+      .eq("id", id);
+
+    if (error) {
+      throw error;
+    }
+  } catch (error) {
+    console.error("Unexpected error:", error);
+    throw error;
+  }
 };
