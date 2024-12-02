0c83b0bf786cba3dad7ae2542787fcc2998b27d1
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-02 19:39:37 +0900

    fix: サイドバーがログインしてないと履歴が表示されないように変更

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,15 +1,19 @@
 import { ArticleHistory } from "@/components/layout/sidebar/ArticleHistory";
+import { currentUser } from "@/lib/auth/currentUser/server";
 import { Suspense } from "react";
 
-export default function Sidebar() {
+export default async function Sidebar() {
+  const { user } = await currentUser();
   return (
     <>
       <aside className="sidebar md:flex hidden w-1/4 max-w-xs overflow-y-auto h-[calc(100vh-95px)] fixed top-24 border-r border-gray-300">
         <div className="w-full p-4">
           <h2>履歴</h2>
-          <Suspense>
-            <ArticleHistory />
-          </Suspense>
+          {user && (
+            <Suspense>
+              <ArticleHistory />
+            </Suspense>
+          )}
         </div>
       </aside>
     </>
