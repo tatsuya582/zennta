27eb74d6e0cdb010e2f51cae4174a7da2c6bb810
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-02 18:18:02 +0900

    add: ルートページに検索フォームを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import ArticleList from "@/components/layout/main/ArticleList";
 import QiitaArticleListSkeleton from "@/components/layout/skeleton/QiitaArticleListSkeleton";
 import ZennArticleListSkeleton from "@/components/layout/skeleton/ZennArticleListSkeleton";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 export default async function Home({
@@ -14,8 +17,21 @@ export default async function Home({
   const qiitaPage = searchParams?.qiitapage || "1";
   const zennPage = searchParams?.zennpage || "1";
 
+  const onsubmit = async (formData: FormData) => {
+    "use server";
+    const name = formData.get("name")?.toString().trim() || "";
+    const encodedName = encodeURIComponent(name);
+    redirect(`/search?query=${encodedName}`);
+  };
+
   return (
     <>
+      <div className="flex items-center justify-center my-12">
+        <form action={onsubmit} className="flex gap-4 w-9/12 max-w-screen-sm">
+          <Input type="name" name="name" />
+          <Button>検索</Button>
+        </form>
+      </div>
       <div className="w-full flex justify-center items-center flex-col md:mt-2 mt-8">
         <h2>Qiita一覧</h2>
         <div className="w-full md:border border-y md:rounded-lg rounded-none p-2 mt-2 border-gray-300">
