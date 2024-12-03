19dfe1e752759ac196bfff0931c7046f80de4672
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-03 22:10:28 +0900

    add: 検索フォームの見た目を調整

--- src/components/layout/form/searchForm.tsx
+++ src/components/layout/form/searchForm.tsx
@@ -17,9 +17,9 @@ export default function SearchForm({
   };
 
   return (
-    <div className="flex items-center justify-center my-12">
-      <form action={onsubmit} className="flex gap-4 w-9/12 max-w-screen-sm">
-        <Input type="name" name="name" defaultValue={query} />
+    <div className="flex items-center justify-center mt-8 mb-4">
+      <form action={onsubmit} className="flex gap-2 md:w-9/12 w-full mx-2 max-w-screen-sm">
+        <Input type="name" name="name" placeholder="検索ワードを入力" defaultValue={query} />
         <Button>検索</Button>
       </form>
     </div>
