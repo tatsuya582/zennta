1b0ee7879dbdea9cfce09c0ec8049044c06fb23a
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-03 20:56:53 +0900

    fix: ページネーションをスクロールバーの外に設定

--- src/components/layout/skeleton/QiitaArticleListSkeleton.tsx
+++ src/components/layout/skeleton/QiitaArticleListSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default async function QiitaArticleListSkeleton() {
   return (
-    <div className="mt-4 h-screen overflow-y-auto scrollbar">
+    <div>
       <div className="flex justify-center border-b border-gray-300 mb-2 pb-4 gap-4">
         <Skeleton className="w-8 h-8" />
         <Skeleton className="w-8 h-8" />
@@ -11,16 +11,18 @@ export default async function QiitaArticleListSkeleton() {
         <Skeleton className="w-8 h-8" />
         <Skeleton className="w-8 h-8" />
       </div>
-      {[...Array(29)].map((_, index) => (
-        <div key={index} className="border-b border-gray-300 mb-2 pb-1">
-          <Skeleton className="w-11/12 h-8 mx-auto my-2" />
-          <div className="flex gap-2 flex-wrap w-11/12 mx-auto pl-2">
-            <Skeleton className="w-24 h-7 px-3" />
-            <Skeleton className="w-24 h-7 px-3" />
-            <Skeleton className="w-24 h-7 px-3" />
+      <div className="mt-4 h-screen overflow-y-auto scrollbar">
+        {[...Array(29)].map((_, index) => (
+          <div key={index} className="border-b border-gray-300 mb-2 pb-1">
+            <Skeleton className="w-11/12 h-8 mx-auto my-2" />
+            <div className="flex gap-2 flex-wrap w-11/12 mx-auto pl-2">
+              <Skeleton className="w-24 h-7 px-3" />
+              <Skeleton className="w-24 h-7 px-3" />
+              <Skeleton className="w-24 h-7 px-3" />
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
       <div className="flex justify-center border-b border-gray-300 mb-2 pb-4 gap-4">
         <Skeleton className="w-8 h-8" />
         <Skeleton className="w-8 h-8" />
