bd4f3cf5a52a62db3feea78265f4bcbcdb2e4c76
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-04 19:10:31 +0900

    fix: 後で読むページのお気に入りボタンが2つあるのでそれのバグを修正
    useStateとkeyを使って削除するidを両方のボタンが持てるように修正

--- src/components/layout/main/StoredArticleList.tsx
+++ src/components/layout/main/StoredArticleList.tsx
@@ -1,6 +1,4 @@
-import { addStoredFavorite, deleteFavorite } from "@/actions/favorite";
 import { addStoredItemHistory } from "@/actions/history";
-import { ActionButton } from "@/components/layout/button/ActionButton";
 import { AddFavoriteColumnButton } from "@/components/layout/button/AddFavoriteColumnButton";
 import { FavoritePageDeleteButton } from "@/components/layout/button/FavoritePageDeleteButton";
 import { ReadLaterPageButton } from "@/components/layout/button/ReadLaterPageButton";
@@ -60,17 +58,7 @@ export default async function StoredArticleList({
                   </div>
                 </div>
               ) : (
-                <div className="flex items-center gap-2">
-                  <ReadLaterPageButton item={item} />
-                  <ActionButton
-                    item={item}
-                    id={item.other_column_id}
-                    isTable={item.is_in_other_table}
-                    tableName="favorite"
-                    deleteAction={deleteFavorite}
-                    addAction={addStoredFavorite}
-                  />
-                </div>
+                <ReadLaterPageButton item={item} />
               )}
             </div>
           </div>
