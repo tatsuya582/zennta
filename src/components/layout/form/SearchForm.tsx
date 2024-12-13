7e3a7406855b5d853e8708c9ff840493663e9967
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-13 19:00:38 +0900

    fix: SearchFormをクライアントサイドで実行されるように変更

--- src/components/layout/form/SearchForm.tsx
+++ src/components/layout/form/SearchForm.tsx
@@ -1,6 +1,9 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import LoadingButton from "@/components/layout/button/LoadingButton";
 import { Input } from "@/components/ui/input";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { type FormEvent, useState } from "react";
 
 export default function SearchForm({
   query = "",
@@ -11,22 +14,39 @@ export default function SearchForm({
   linkPage: "search" | "favorite" | "readlater";
   isNoQuery?: boolean;
 }) {
+  const router = useRouter();
+  const [name, setName] = useState(query);
+  const [isLoading, setIsLoading] = useState(false);
   const layoutClass = isNoQuery
     ? "h-[80vh] flex flex-col items-center justify-center mx-2 gap-8"
     : "flex items-center justify-center mt-8 mb-4";
-  const onsubmit = async (formData: FormData) => {
-    "use server";
-    const name = formData.get("name")?.toString().trim() || "";
-    const encodedName = encodeURIComponent(name);
-    redirect(`/${linkPage}?query=${encodedName}`);
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (query === name.trim()) {
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(true);
+    const encodedName = encodeURIComponent(name.trim());
+    router.push(`/${linkPage}?query=${encodedName}`);
   };
 
   return (
     <div className={layoutClass}>
       {isNoQuery && <h2>検索</h2>}
-      <form action={onsubmit} className="flex gap-2 md:w-9/12 w-full mx-2 max-w-screen-sm">
-        <Input type="name" name="name" placeholder="検索ワードを入力" defaultValue={query} />
-        <Button>検索</Button>
+      <form onSubmit={handleSubmit} className="flex gap-2 md:w-9/12 w-full mx-2 max-w-screen-sm">
+        <Input
+          type="text"
+          name="name"
+          placeholder="検索ワードを入力"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <div>
+          <LoadingButton isLoading={isLoading} loadingMx="mx-1">
+            検索
+          </LoadingButton>
+        </div>
       </form>
       {isNoQuery && <p className="text-gray-500 text-sm mt-4">なにか入力してください</p>}
     </div>
