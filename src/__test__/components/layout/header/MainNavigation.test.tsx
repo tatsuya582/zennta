7883b97070997dde7d4483a85330544a6106c997
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-18 19:05:04 +0900

    add: componentsのMainNabigationの単体テストを作成

---
+++ src/__test__/components/layout/header/MainNavigation.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { useRouter } from "next/router";
+import MainNavigation from "@/components/layout/header/MainNavigation";
+import { getUser } from "@/actions/user";
+
+jest.mock("@/actions/user", () => ({
+  getUser: jest.fn(),
+}));
+
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(),
+}));
+
+describe("MainNavigation", () => {
+  beforeEach(() => {
+    (useRouter as jest.Mock).mockReturnValue({
+      pathname: "/",
+    });
+  });
+
+  it("should render navigation links", () => {
+    (getUser as jest.Mock).mockResolvedValueOnce({ name: "test" });
+    render(<MainNavigation />);
+
+    const searchLink = screen.getByText("検索");
+    expect(searchLink).toBeInTheDocument();
+    expect(searchLink).toHaveAttribute("href", "/search");
+
+    const readLaterLink = screen.getByText("後で読む");
+    expect(readLaterLink).toBeInTheDocument();
+    expect(readLaterLink).toHaveAttribute("href", "/readlater");
+
+    const favoriteLink = screen.getByText("お気に入り");
+    expect(favoriteLink).toBeInTheDocument();
+    expect(favoriteLink).toHaveAttribute("href", "/favorite");
+  });
+});
