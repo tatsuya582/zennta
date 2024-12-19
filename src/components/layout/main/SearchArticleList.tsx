1a4f6181b2f545a3c1fd6ec3c96a9922b64e21d4
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-19 21:44:07 +0900

    remove: コメントアウトの消し忘れを削除

--- src/components/layout/main/SearchArticleList.tsx
+++ src/components/layout/main/SearchArticleList.tsx
@@ -47,40 +47,5 @@ export default async function SearchArticleList({ query, currentPage, otherPage,
       favoriteUrls={favoriteUrls}
       isLogin={!!user}
     />
-    // <div>
-    //   <div className="border-b border-gray-300 my-2 pb-4">{pagination}</div>
-    //   <div className="mt-2">
-    //     {articles.map((item) => (
-    //       <div key={item.id} className="border-b border-gray-300 m-2 pb-1">
-    //         <div className="flex md:flex-row flex-col justify-between gap-1">
-    //           <Article item={item} onSubmit={addHistory} />
-    //           {isLogin && (
-    //             <div className="flex items-center gap-2">
-    //               <ActionButton
-    //                 item={item}
-    //                 id={readLaterUrls.get(item.url)}
-    //                 isTable={readLaterUrls.has(item.url)}
-    //                 tableName="readLater"
-    //                 deleteAction={deleteReadLater}
-    //                 addAction={addreadLater}
-    //                 key={readLaterUrls.get(item.url)}
-    //               />
-    //               <ActionButton
-    //                 item={item}
-    //                 id={favoriteUrls.get(item.url)}
-    //                 isTable={favoriteUrls.has(item.url)}
-    //                 tableName="favorite"
-    //                 deleteAction={deleteFavorite}
-    //                 addAction={addFavorite}
-    //                 key={favoriteUrls.get(item.url)}
-    //               />
-    //             </div>
-    //           )}
-    //         </div>
-    //       </div>
-    //     ))}
-    //   </div>
-    //   <div className="mt-4 mb-2">{pagination}</div>
-    // </div>
   );
 }
