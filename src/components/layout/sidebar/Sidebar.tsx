cde33de1688412d745d465b8b42202d796b82689
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-12-21 18:01:14 +0900

    add: componentsのSidebarの単体テストを作成

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -4,7 +4,10 @@ import { Suspense } from "react";
 export const Sidebar = () => {
   return (
     <>
-      <aside className="scrollbar md:flex hidden w-1/4 max-w-xs overflow-y-auto h-[calc(100vh-95px)] fixed top-24 border-r border-gray-300">
+      <aside
+        className="scrollbar md:flex hidden w-1/4 max-w-xs overflow-y-auto h-[calc(100vh-95px)] fixed top-24 border-r border-gray-300"
+        aria-label="履歴"
+      >
         <div className="w-full p-4">
           <h2>履歴</h2>
           <Suspense>
