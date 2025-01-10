269127af03a82ba35efe212321d0aed4f3d6caf4
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-01-10 23:56:10 +0900

    add: グループ作成用にフォームを仮作成

---
+++ src/components/layout/form/useCreateGroupForm.ts
@@ -0,0 +1,62 @@
+import { useToast } from "@/hooks/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm, UseFormReturn } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  name: z.string().max(40, {
+    message: "メモは40文字以下にしてください",
+  }),
+});
+
+type useCreateGroupFormProps = {
+  form: UseFormReturn<z.infer<typeof formSchema>>;
+  onSubmit: (values: z.infer<typeof formSchema>) => Promise<void>;
+  isLoading: boolean;
+};
+type article = {
+  id: string;
+  title: string;
+};
+
+export const useCreateGroupForm = (
+  initialName: string,
+  articles: article[],
+  initialArticles = [],
+  isEdit = false
+): useCreateGroupFormProps => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: initialName,
+    },
+  });
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const toastStr = isEdit ? "お気に入りグループを編集しました" : "お気に入りグループを作成しました";
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      if (isEdit && initialName === values.name && initialArticles === articles) {
+        return;
+      }
+      const name = values.name || "無題";
+      setIsLoading(true);
+      console.log("title: ", name);
+      console.log(articles);
+      router.refresh();
+      toast({
+        description: toastStr,
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { form, onSubmit, isLoading };
+};
