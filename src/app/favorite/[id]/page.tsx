cb4eb339f96d1d291c2829099c69158fcb62a33b
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-01-11 16:51:20 +0900

    add: お気に入りグループの詳細ページを仮作成

---
+++ src/app/favorite/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "お気に入りグループ",
+};
+
+export default function FavoriteGroupPage({ params } : { params: { id: string } }) {
+  
+  return (
+    <>
+      <div className="w-full flex justify-center items-center flex-col md:mt-2 mt-8 mb-4">
+        <h2>お気に入りグループ</h2>
+        <div className="w-full">
+          {params.id}
+        </div>
+      </div>
+    </>
+  );
+}
