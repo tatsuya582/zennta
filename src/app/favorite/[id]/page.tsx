b12b7ab5af56f2369bd3117855a558c27a4e98ec
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-01-14 12:08:12 +0900

    add: お気に入りグループの詳細ページもお気に入りページへのリンクを作成

--- src/app/favorite/[id]/page.tsx
+++ src/app/favorite/[id]/page.tsx
@@ -3,6 +3,8 @@ import { ArticleListSkeleton } from "@/components/layout/skeleton/ArticleListSke
 import { GroupArticleList } from "@/components/layout/group/GroupArticleList";
 import { Suspense } from "react";
 import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { type Metadata } from "next";
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
@@ -22,10 +24,18 @@ export default async function FavoriteGroupPage({ params }: { params: { id: stri
   }
   return (
     <>
+      <div className="flex justify-end md:mt-0 mt-4">
+        <Button variant="outline">
+          <Link href="/favorite">戻る</Link>
+        </Button>
+      </div>
       <div className="w-full flex justify-center items-center flex-col md:mt-2 mt-8 mb-4">
         <h2>{title}</h2>
         {articles && articles.length !== 0 && (
-          <div className="w-full md:border border-y md:rounded-lg rounded-none p-2 mt-2 border-gray-300">
+          <div
+            className="w-full md:border border-y md:rounded-lg rounded-none p-2 mt-2 border-gray-300"
+            data-testid="favorite-group"
+          >
             <Suspense fallback={<ArticleListSkeleton />}>
               <GroupArticleList articles={articles} />
             </Suspense>
