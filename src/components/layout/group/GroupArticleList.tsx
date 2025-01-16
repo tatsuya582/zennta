25c0951206aa833763699101c68d94ff1a60a039
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-01-16 22:03:01 +0900

    add: グループ詳細ページにラインが1本足りなかったので追加

--- src/components/layout/group/GroupArticleList.tsx
+++ src/components/layout/group/GroupArticleList.tsx
@@ -6,6 +6,7 @@ import { type FetchedArticles } from "@/types/databaseCustom.types";
 export const GroupArticleList = ({ articles }: { articles: FetchedArticles[] }) => {
   return (
     <div className="mt-4" data-testid="favorite-group-detail">
+      <div className="border-b border-gray-300 m-2 pb-1" />
       {articles.map((item, index) => {
         return (
           <div key={item.id} className="border-b border-gray-300 m-2 pb-1">
