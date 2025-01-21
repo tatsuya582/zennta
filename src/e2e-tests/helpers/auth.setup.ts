46b195c8f114d3a3c68bc21b8874ada3853379bb
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2025-01-21 22:10:39 +0900

    add: E2EテストのsetupでOAuthを使わないように変更

--- src/e2e-tests/helpers/auth.setup.ts
+++ src/e2e-tests/helpers/auth.setup.ts
@@ -1,28 +1,17 @@
 import { test as setup, expect } from "@playwright/test";
 import path from "path";
 import dotenv from "dotenv";
+import { getHeaderLocator } from "@/e2e-tests/helpers/locator";
 
 const authFile = path.join(__dirname, "../.auth/user.json");
 dotenv.config({ path: [".env.local", ".env"] });
 
 setup("authenticate", async ({ page }) => {
-  await page.goto("/signup");
-  await page.locator('button:has-text("GitHubで会員登録")').click();
-  await expect(page.locator("text=continue to Zennta_local")).toBeVisible();
-  await page.fill('input[name="login"]', process.env.NEXT_PUBLIC_TEST_USER!);
-  await page.fill('input[name="password"]', process.env.NEXT_PUBLIC_TEST_PASSWORD!);
-  await page.click('input[name="commit"]');
+  await page.goto("/testlogin");
+  await page.locator("button", { hasText: "ログイン" }).click();
 
-  await page
-    .waitForSelector('button:has-text("Authorize")', { timeout: 5000 })
-    .then(async () => {
-      await page.click('button:has-text("Authorize")');
-    })
-    .catch(() => {
-      // ボタンがなければ何もしない
-    });
-  const header = await page.getByTestId("header");
-  await expect(header.locator("text=Zennta")).toBeVisible({ timeout: 30000 });
+  const header = await getHeaderLocator(page);
+  await expect(header.locator("button", { hasText: "ログアウト" })).toBeVisible({ timeout: 30000 });
 
   await page.context().storageState({ path: authFile });
 });
